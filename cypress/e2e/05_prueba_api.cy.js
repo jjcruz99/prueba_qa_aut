b342aabc1a4cd5cb7d114765b8a1438bfa3dc1ec
jjcruz99 <jjcruz139@gmail.com>
2025-12-28 12:17:58 -0500

    Pruebas de la API con cypress y se optimiza las pruebas de la api en JS

--- cypress/e2e/05_prueba_api.cy.js
+++ cypress/e2e/05_prueba_api.cy.js
@@ -6,16 +6,53 @@ describe('Prueba de la API reqres.in/api/ (Flujo Completo)', () => {
         "job": "Automation Engineer"
     };
 
-    // Define los headers requeridos por la API.
-    
     const headers = {
         // Añadimos el header de la documentación:
-        'x-api-key': 'YOUR_API_KEY', 
+        'x-api-key': 'dev_2551a7cca1a0e86bef88f4480581b7e81768470e95aaaf79', 
         'Content-Type': 'application/json' // Especifica el tipo de cuerpo para el POST
     };
 
-    it('Flujo completo: Crear usuario (POST) y obtenerlo por ID (GET)', () => { 
 
+    it('Validar que la colección existe (GET)', () => { 
+       
+        cy.request({
+            method: 'GET',
+            url: `${url}/collections/collection-1`, 
+            headers: headers
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body).to.have.property('data');
+        });
+    });
+
+
+    it('Validar que el recods de la coleccion responde correctamente (GET)', () => { 
+       
+        cy.request({ 
+            method: 'GET',
+            url: `${url}/collections/collection-1/records/82d60b7d-c029-470e-9dae-a4cdea14dae0`, 
+            headers: headers
+        }).then((response) => { 
+            expect(response.status).to.eq(200);
+            expect(response.body).to.have.property('data');
+            expect(response.body.data).to.have.property('id', '82d60b7d-c029-470e-9dae-a4cdea14dae0'); 
+        });
+
+    });
+
+    it('Flujo completo: Crear usuario (POST) y obtenerlo el ID ', () => { 
+        cy.request({
+            method:'POST',
+            url:`${url}/users`,
+            headers : headers,
+            body: usuario
+        }).then((response)=>{
+            expect(response.status).to.eq(201);
+            expect(response.body).to.have.property('id');
+            //extraer el id generado
+            const idGenerado = response.body.id;
+            cy.log(`Id generado por la api ${idGenerado}`)
+        });
     });
   
 });
--- src/pruebas_api/api.js
+++ src/pruebas_api/api.js
@@ -1,41 +1,70 @@
-async function obtenerColeccion(){
-    const response = await fetch('https://reqres.in/api/collections/collection-1', {
-    headers: { 'x-api-key': 'dev_2551a7cca1a0e86bef88f4480581b7e81768470e95aaaf79' },
-    });
-    const data = await response.json();
-    console.log(data);
-    
+/**
+ * Obtiene una coleccion de los tres endpoints de prueba los tres metodos retorna un JSON.
+ * @returns {json} data - La coleccion obtenida desde el endpoint.
+ */
+
+
+export async function obtenerColeccion(){
+    try{
+        const response = await fetch('https://reqres.in/api/collections/collection-1', {
+        headers: { 'x-api-key': 'dev_2551a7cca1a0e86bef88f4480581b7e81768470e95aaaf79' },
+        });
+
+        if (response.status !== 200) {
+            throw new Error(`Error en la solicitud: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }
+    catch(error){
+        console.error('Error al obtener la coleccion: ', error);
+        return null;
+    }
 }
 
-async function obtenerRecordsPorId(){
-    const response = await fetch('https://reqres.in/api/collections/collection-1/records/82d60b7d-c029-470e-9dae-a4cdea14dae0', {
+export async function obtenerRecordsPorId(){
+    try{
+        const response = await fetch('https://reqres.in/api/collections/collection-1/records/82d60b7d-c029-470e-9dae-a4cdea14dae0', {
     headers: { 'x-api-key': 'dev_pub_a145e1fae115de05afc1dc61454e15f0' },
     });
     const respuestaEnFormatoJSON = await response.json();
     console.log(respuestaEnFormatoJSON);
     //console.log(`Record obtenido con tags: ${respuestaEnFormatoJSON.data.data.tags}`);
+    return respuestaEnFormatoJSON;
+    }
+    catch(error){
+        console.error('Error al obtener el record por ID: ', error);
+        return null;
+    }
 }
 
-async function crearUsuario(){
-
-    const usuario = {
+export async function crearUsuario(){
+    try{
+        const usuario = {
                     "name": "Test User", 
                     "job": "Automation Engineer"
                     };
-    const response = await fetch('https://reqres.in/api/users', {
-        method: 'POST',
-        headers: {  
-            'Content-Type': 'application/json',
-            'x-api-key': 'dev_2551a7cca1a0e86bef88f4480581b7e81768470e95aaaf79'
-        },
-        body: JSON.stringify(usuario)
-    });
+        const response = await fetch('https://reqres.in/api/users', {
+            method: 'POST',
+            headers: {  
+                'Content-Type': 'application/json',
+                'x-api-key': 'dev_2551a7cca1a0e86bef88f4480581b7e81768470e95aaaf79'
+            },
+            body: JSON.stringify(usuario)
+        });
 
-    const data = await response.json();
-    console.log(data);
-    console.log(`Usuario creado con ID: ${data.id}`);
+        const data = await response.json();
+        console.log(data);
+        console.log(`Usuario creado con ID: ${data.id}`);
+        return data;
+    }
+    catch(error){
+        console.error('Error al crear el usuario: ', error);
+        return null;
+    }
 }
 
-//obtenerColeccion();
+obtenerColeccion();
 obtenerRecordsPorId();
-//crearUsuario();
+crearUsuario();
